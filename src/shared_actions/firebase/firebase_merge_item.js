f412d66c03056200833909011dafc23e337ce821
Julio Makdisse Saito <saitodisse@gmail.com>
2016-11-30 12:38:35 -0200

    fix(firebase): fix firebase merge

--- src/shared_actions/firebase/firebase_merge_item.js
+++ src/shared_actions/firebase/firebase_merge_item.js
@@ -2,9 +2,7 @@ function firebase_merge_item(state_path) {
   return function mergeItem({ state, input }) {
     const { key, value } = input;
     const exists = state.get(state_path) && state.get(`${state_path}.${key}`);
-    if (exists) {
-      state.set(`${state_path}.${key}`, value);
-    }
+    state.set(`${state_path}.${key}`, value);
     return { key };
   };
 }
